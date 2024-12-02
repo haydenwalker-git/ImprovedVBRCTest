373d8ed16b1d0e4163de2fcc31101f374a3f5746
Hayden Walker <haydenwalker04@gmail.com>
2024-12-02 14:08:10 -0600

    changed inheritance of DynamicItem from ICraftable to IDynamicItem for testing

--- ImprovedVBRCTest/DynamicItem.cs
+++ ImprovedVBRCTest/DynamicItem.cs
@@ -6,7 +6,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-public class DynamicItem : ICraftable
+public class DynamicItem : IDynamicItem
 {
 
     private const int MinQualityLevel = 1;
