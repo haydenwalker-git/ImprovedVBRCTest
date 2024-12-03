6a7961ef2e07541f8e48a91669756affe13dd224
Hayden Walker <haydenwalker04@gmail.com>
2024-12-03 13:44:04 -0600

    Deleted UpgradeableItemDabase and ItemDatabase

--- ImprovedVBRCTest/obj/Debug/net8.0/ImprovedVBRCTest.AssemblyInfo.cs
+++ ImprovedVBRCTest/obj/Debug/net8.0/ImprovedVBRCTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImprovedVBRCTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa052c0cd051e55fb44c05ea91bf3662b227ed1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+791d7eaac98028875c95fabafef184f7e7691678")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImprovedVBRCTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImprovedVBRCTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
